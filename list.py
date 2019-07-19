1a6f02ff51197b128ebb80d5a923f3db667fcdfa
Pluto <plutoef@gmail.com>
2019-07-19 17:06:12 +0800

    List 練習

---
+++ list.py
@@ -0,0 +1,12 @@
+
+a = ['Toyota', 'Honda', 123, True]
+print(a)
+print(a[0])
+print(a[3])
+print(len(a))
+
+a.append('Audi')
+print(a)
+print(len(a))
+print('Audi' in a) # 變成是非題True or False
+print('benz' in a) 
