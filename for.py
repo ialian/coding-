f8614ed4bf38c04b2b432c1a02eb11a60262a1da
Pluto <plutoef@gmail.com>
2019-07-19 17:17:34 +0800

    for loop 練習

---
+++ for.py
@@ -0,0 +1,14 @@
+# for loop
+
+cars = ['Toyota', 'Honda',123]
+
+for car in cars:
+	print(car)
+
+# for loop 意思就是把清單中的東西一個一個拿出來
+# 上例就是把cars(清單中的東西)一個一個拿出來，
+#  每一個東西稱呼為car(暫時性變數)。
+
+names = ['Jack', 'Tommy', 'people']
+for name in names:
+	print(name)
